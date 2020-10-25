536c3788611131bea7aa0a60145ee8c6c44f8d0d
Mathew Thomas <53473163+JoJoMathew@users.noreply.github.com>
2020-10-25 13:17:57 +0000

    Create Storyboard & Controllers

---
+++ SearchMov/SearchMov/ViewControllers/DetailController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailController.swift
+//  SearchMov
+//
+//  Created by Mathew Thomas on 25/10/2020.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
